53b2a26463fadc0e87b714d1b1d3bcc79ddb7ec9
Alexandru Pinca <apinca@mubi.com>
2018-08-09 11:27:59 +0100

    Use MUBI's version of pycaption

--- setup.py
+++ setup.py
@@ -6,19 +6,23 @@ README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                            'README.md')
 
 dependencies = [
-    'pycaption'
+  'pycaption>=1.1.0'
 ]
 
 setup(
-    name='pycaption-cli',
-    version='0.1',
-    description='Command line caption conversion',
-    author='Joe Norton',
-    author_email='joey@nortoncrew.com',
-    url='https://github.com/jnorton001/pycaption-cli',
-    install_requires=dependencies,
-    packages=find_packages(),
-    include_package_data=True,
-    entry_points=dict(
-        console_scripts=['pycaption=pycapcli.caption_converter:main']),
+  name='pycaption-cli',
+  version='0.1',
+  description='Command line caption conversion',
+  author='Joe Norton',
+  author_email='joey@nortoncrew.com',
+  url='https://github.com/jnorton001/pycaption-cli',
+  install_requires=dependencies,
+  dependency_links=[
+    'https://github.com/mubi/pycaption/tarball/master#egg=pycaption-1.1.0'
+  ],
+  packages=find_packages(),
+  include_package_data=True,
+  entry_points=dict(
+    console_scripts=['pycaption=pycapcli.caption_converter:main']
+  ),
 )

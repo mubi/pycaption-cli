860939e8eb492ae440e605c751bd9c206c9a8414
Alexandru Pinca <apinca@mubi.com>
2019-05-03 12:22:45 +0100

    Bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -6,19 +6,19 @@ README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
                            'README.md')
 
 dependencies = [
-  'pycaption>=1.1.0'
+  'pycaption>=1.2.0'
 ]
 
 setup(
   name='pycaption-cli',
-  version='0.3',
+  version='0.3.1',
   description='Command line caption conversion',
   author='Joe Norton',
   author_email='joey@nortoncrew.com',
   url='https://github.com/jnorton001/pycaption-cli',
   install_requires=dependencies,
   dependency_links=[
-    'https://github.com/mubi/pycaption/tarball/master#egg=pycaption-1.1.0'
+    'https://github.com/mubi/pycaption/tarball/master#egg=pycaption-1.2.0'
   ],
   packages=find_packages(),
   include_package_data=True,
